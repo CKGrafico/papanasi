4976f867294deaa16af2cf8925b1a442ae78271d
Quique Fdez Guerra <quique@ckgrafico.com>
2022-05-27 08:49:39 +0300

    Add shadowdom

--- src/button/button.lite.tsx
+++ src/button/button.lite.tsx
@@ -1,7 +1,11 @@
+import { useMetadata } from '@builder.io/mitosis';
+
 export type ButtonProps = {
   variant?: string;
   children?: any; // TODO change
 };
+
+useMetadata({ isAttachedToShadowDom: true });
 
 export default function Button(props: ButtonProps) {
   return <button class={'pa-button ' + props.variant ? 'pa-button--' + props.variant : ''}>{props.children}</button>;
