44428c78694f43b48aef3b859cdf6337f7b9220c
Quique Fdez Guerra <quique@ckgrafico.com>
2022-05-28 08:34:32 +0300

    move to mdx

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,11 +1,10 @@
 module.exports = {
-  stories: ['../docs/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../docs/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-docs',
-    '@storybook/addon-links',
     {
       name: '@storybook/addon-postcss',
       options: {
