d4a0905f0d9f7a6c49a02ca32dd791e4bc3db6d0
Quique Fdez Guerra <quique@ckgrafico.com>
2022-05-30 22:18:50 +0300

    change hidden for 0

--- src/column/column.lite.tsx
+++ src/column/column.lite.tsx
@@ -6,7 +6,7 @@ import './column.css';
 export type ColumnProps = {
   debug?: boolean;
   children?: Children;
-} & BreakpointProps<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 'auto' | 'hidden'>;
+} & BreakpointProps<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 'auto'>;
 
 useMetadata({ isAttachedToShadowDom: true });
 
