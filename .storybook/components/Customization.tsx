6c1bd61c7b8b806b05e47d00fe18ee8668a9c0d2
Quique Fdez Guerra <quique@ckgrafico.com>
2022-06-02 13:19:17 +0300

    Init custom css

--- .storybook/components/Customization.tsx
+++ .storybook/components/Customization.tsx
@@ -1,9 +1,38 @@
 import React, { useMemo, useState } from 'react';
 import { Column, Container, Row } from '../../packages/react';
+import './customization.css';
+
+type CustomizationProps = {
+  css?: string;
+};
+
+const defaultCss = `
+:root {
+  /* Check all the variables at https://github.com/CKGrafico/papanasi/blob/main/styles/variables.css */
+    --pa-breakpoint-xxs: 360px;
+    --pa-breakpoint-xs: 640px;
+    --pa-breakpoint-s: 768px;
+    --pa-breakpoint-m: 1024px;
+    --pa-breakpoint-l: 1280px;
+    --pa-breakpoint-xl: 1440px;
+    --pa-breakpoint-xxl: 1920px;
+
+    --pa-color-basic-brightest: #ffffff;
+    --pa-color-basic-darkest: #101116;
+    --pa-color-primary-normal: #017aff;
+    --pa-color-secondary-normal: #6c47ff;
+    --pa-color-tertiary-normal: #f34971;
+
+  /* ... */
+}
+`;
 
 // TODO: Use our select in the future?
-export function Customization() {
+export function Customization(props: CustomizationProps) {
+  const { css = defaultCss } = props;
+
   const [selected, setSelected] = useState('papanasi');
+  const [customCss, setCustomCss] = useState('');
 
   const themes = [
     {
@@ -24,9 +53,14 @@ export function Customization() {
     setSelected(event.target.value);
   }
 
+  function onChangeCss(event) {
+    setCustomCss(event.target.value);
+  }
+
   return (
-    <Container>
+    <Container className="customization">
       {stylePath && <link rel="stylesheet" type="text/css" href={stylePath} />}
+      <style>{customCss}</style>
 
       <Row>
         <Column xs={'content'}>Choose a theme</Column>
@@ -40,6 +74,20 @@ export function Customization() {
           </select>
         </Column>
       </Row>
+      {css && selected !== 'none' && (
+        <>
+          <Row>
+            <Column xs={'content'}>Customize CSS properties</Column>
+          </Row>
+          <Row>
+            <Column xs={'fill'}>
+              <textarea className="customization__code" onChange={onChangeCss}>
+                {css}
+              </textarea>
+            </Column>
+          </Row>
+        </>
+      )}
     </Container>
   );
 }
