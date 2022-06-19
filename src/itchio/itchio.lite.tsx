076b1016d6794190062f15b63917d8979887dc63
Quique Fdez Guerra <quique@ckgrafico.com>
2022-06-19 14:10:33 +0300

    Fix itchio

--- src/itchio/itchio.lite.tsx
+++ src/itchio/itchio.lite.tsx
@@ -21,23 +21,15 @@ export default function Itchio(props: ItchioProps) {
   const state = useState({
     classes: '',
     isScriptLoaded: false,
+    isLoadingGameInfo: false,
     gameInfo: null,
-    onLoadGame() {
-      // Cannot move outside because the Refs lost 'this'
-      function onLoadGameInfo() {
-        if (!state.gameInfo) {
-          return;
-        }
-
-        global.Itch.attachBuyButton(actionRef, {
-          user: props.user,
-          game: props.game,
-          width: props.width || 800,
-          height: props.height || 600
-        });
-      }
-
-      onLoadGameInfo();
+    attachButton() {
+      global.Itch.attachBuyButton(actionRef, {
+        user: props.user,
+        game: props.game,
+        width: props.width || 800,
+        height: props.height || 600
+      });
     }
   });
 
@@ -58,15 +50,18 @@ export default function Itchio(props: ItchioProps) {
 
   onUpdate(() => {
     const onLoadScript = () => {
-      if (!state.isScriptLoaded) {
+      if (!state.isScriptLoaded || state.isLoadingGameInfo) {
         return;
       }
+
+      state.isLoadingGameInfo = true;
 
       global.Itch.getGameData({
         user: props.user,
         game: props.game,
         secret: props.secret,
         onComplete: (data) => {
+          state.attachButton();
           state.gameInfo = data;
           props.onLoad && props.onLoad();
         }
@@ -76,26 +71,23 @@ export default function Itchio(props: ItchioProps) {
     onLoadScript();
   }, [state.isScriptLoaded]);
 
-  onUpdate(() => state.onLoadGame(), [state.gameInfo]);
-
   return (
     <div className={state.classes}>
-      {!state.gameInfo && <span>Loading...</span>}
-
-      {state.gameInfo && (
-        <div className="pa-itchio__container">
-          <img className="pa-itchio__image" src={state.gameInfo.cover_image}></img>
-          <div className="pa-itchio__info">
+      <div className="pa-itchio__container">
+        {state.gameInfo && <img className="pa-itchio__image" src={state.gameInfo.cover_image}></img>}
+        <div className="pa-itchio__info">
+          {state.gameInfo && (
             <div className="pa-itchio__texts">
               <span className="pa-itchio__title">{state.gameInfo.title}</span>
               <span className="pa-itchio__price">{state.gameInfo.price}</span>
             </div>
-            <span className="pa-itchio__children" ref={actionRef}>
-              {props.children}
-            </span>
-          </div>
+          )}
+          <span className="pa-itchio__children" ref={actionRef}>
+            {!state.gameInfo && <span>Loading...</span>}
+            {state.gameInfo && <span>{props.children}</span>}
+          </span>
         </div>
-      )}
+      </div>
     </div>
   );
 }
