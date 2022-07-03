9e9902f6e73e52a4f86c459c45981251dc07d03f
Quique Fdez Guerra <quique@ckgrafico.com>
2022-07-03 14:35:26 +0300

    Init avatar component

---
+++ src/avatar/avatar.lite.tsx
@@ -0,0 +1,65 @@
+import { onMount, useMetadata, useState } from '@builder.io/mitosis';
+import { classesToString } from '../../../helpers';
+import { Dynamic, SharedProps, Variant } from '../../../models';
+import './button.css';
+
+export type AvatarProps = {
+  variant?: Dynamic<Variant>;
+  name?: string;
+  unavatar?: string;
+  url?: string;
+  disabled?: boolean;
+} & SharedProps;
+
+useMetadata({ isAttachedToShadowDom: true });
+export default function Avatar(props: AvatarProps) {
+  const state = useState({
+    classes: '',
+    src: null,
+    initials: '',
+    autoColor: true
+  });
+
+  onMount(() => {
+    const setInitialProps = (variant, disabled, className) => {
+      state.classes = classesToString([
+        'pa-avatar',
+        [variant, `pa-avatar--${variant}`],
+        [disabled, 'is-disabled'],
+        className || ''
+      ]);
+
+      state.autoColor = variant ? false : state.autoColor;
+    };
+
+    const setNameInitials = (name) => {
+      // From: https://stackoverflow.com/a/63763497/3274609
+      state.initials = name
+        .match(/(^\S\S?|\s\S)?/g)
+        .map((v) => v.trim())
+        .join('')
+        .match(/(^\S|\S$)?/g)
+        .join('')
+        .toLocaleUpperCase();
+    };
+
+    const setSource = (url, unavatar) => {
+      if (unavatar) {
+        state.src = `https://unavatar.io/${unavatar}`;
+        return;
+      }
+
+      state.src = url;
+    };
+
+    setInitialProps(props.variant, props.disabled, props.className);
+    setNameInitials(props.name || '');
+    setSource(props.url, props.unavatar);
+  });
+
+  return (
+    <div className={state.classes} style={{ backgroundImage: state.src }}>
+      {state.initials}
+    </div>
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { initBreakpointChecker } from '../../../helpers';
 import '../../../styles/variables.css';
 
+export { default as Avatar } from './avatar';
 export { default as Button } from './button';
 export { default as Code } from './code';
 export { default as Column } from './column';
@@ -8,7 +9,5 @@ export { default as Container } from './container';
 export { default as Itchio } from './itchio';
 export { default as Pill } from './pill';
 export { default as Row } from './row';
-export { default as Tab } from './tab';
-export { default as Tabs } from './tabs';
 
 initBreakpointChecker();
