f09b79ab6bcd652ac1f7cb2a6dea8368bd83054c
Quique Fdez Guerra <quique@ckgrafico.com>
2022-09-25 16:18:32 +0300

    add watcher

--- compiler/dev.tasks.js
+++ compiler/dev.tasks.js
@@ -1,40 +1,48 @@
 const { Listr } = require('listr2');
+const chokidar = require('chokidar');
+const path = require('path');
+const ora = require('ora');
 
 (async () => {
   const execa = (await import('execa')).command;
+  const { spawn } = require('node:child_process');
 
   const tasks = new Listr([
     {
-      title: 'Compile React Components',
-      task: () =>
-        execa('node ./compiler/targets/react').catch(() => {
-          throw new Error('Error compiling React');
-        })
-    },
-    {
-      title: 'Bundle React',
-      task: () =>
-        execa('yarn lerna --scope=@papanasi/react build').catch(() => {
-          throw new Error('Error bundling React');
-        })
+      title: 'Launch Watcher',
+      task: async () => {
+        return chokidar
+          .watch(['src/**/*', 'helpers/**/*', 'models/**/*', 'styles/**/*'])
+          .on('all', async (event, pathName) => {
+            if (event !== 'change') {
+              return;
+            }
+
+            const file = path.parse(pathName);
+            const name = file.dir.replace('src\\', '');
+            const spinner = ora(`Changed ${name}, compiling... `).start();
+
+            try {
+              await execa(`node ./compiler/targets/react`);
+            } catch (e) {
+              spinner.text = `Error compiling ${e.message}.`;
+              spinner.fail();
+
+              return;
+            }
+
+            spinner.text = 'Compiled successfully.';
+            spinner.succeed();
+          });
+      },
+      options: {
+        persistentOutput: true
+      }
     },
     {
       title: 'Launch Storybook',
       task: () => {
-        execa('yarn start-storybook -p 6006 --no-manager-cache').catch((error) => {
-          throw new Error(error);
-        });
-
-        return true;
-      }
-    },
-    {
-      title: 'Launch Watcher',
-      task: () => {
-        execa('chokidar "src/**/*" "styles/**/*" -c "node ./compiler/targets/react"').catch((error) => {
-          throw new Error(error);
-        });
-
+        execa('yarn start-storybook -p 6006 --no-manager-cache');
         return true;
       }
     }
