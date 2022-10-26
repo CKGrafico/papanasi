0eaa4b73609aa92876957d0af4207e8cc751ed0d
Quique Fdez Guerra <quique@ckgrafico.com>
2022-10-26 20:09:17 +0300

    try to fix svelte onchange

--- compiler/platforms/svelte.js
+++ compiler/platforms/svelte.js
@@ -33,7 +33,9 @@ const DEFAULT_OPTIONS = {
       // Svelte compiler is not adding let to the state values
       .replace(/^  (\w*) = (.*)/gm, '  let $1 = $2')
       // Fix state in svelte
-      .replace(/svelte:component\n.*this=\{circle\}/g, 'circle');
+      .replace(/svelte:component\n.*this=\{circle\}/g, 'circle')
+      // Remove Onchange
+      .replace('import onChange from "on-change";', '');
 
     fs.writeFileSync(outFile, result, 'utf8');
   }
--- helpers/element.helper.ts
+++ helpers/element.helper.ts
@@ -1,17 +1,20 @@
 export function querySelectorAllObservable(
   element: HTMLElement,
   selector: string,
-  callback = (element: HTMLElement) => null
+  callback: (element: HTMLElement) => void
 ) {
   const elements = Array.from(element.querySelectorAll(selector));
-  elements.forEach(callback);
+
+  if (callback) {
+    elements.forEach(callback);
+  }
 
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       mutation.addedNodes.forEach((element: HTMLElement) => {
         if (element.nodeType === Node.ELEMENT_NODE && element.matches(selector)) {
           elements.push(element);
-          callback(element);
+          callback && callback(element);
         }
       });
     });
