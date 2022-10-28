f2eefa7b93814a19363b851fc037688813bc9ed8
Quique Fdez Guerra <quique@ckgrafico.com>
2022-10-28 20:46:56 +0300

    version

--- packages/qwik/vite.config.ts
+++ packages/qwik/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(() => {
       lib: {
         entry: './src/index.ts',
         formats: ['es', 'cjs'],
-        fileName: (format) => `index.qwik.${format === 'es' ? 'mjs' : 'cjs'}`
+        fileName: (format) => `papanasi.qwik.${format === 'es' ? 'mjs' : 'cjs'}`
       },
       minify: false,
       rollupOptions: {
