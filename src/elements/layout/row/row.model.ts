8f7446addf58c6450cd941283d215c5efd240242
Quique Fdez Guerra <quique@ckgrafico.com>
2023-03-19 19:09:43 +0200

    inject dependencies

--- src/elements/layout/row/row.model.ts
+++ src/elements/layout/row/row.model.ts
@@ -1,6 +1,6 @@
 import type { BaseProps, BaseState, BreakpointProps } from '~/models';
 
-export type RowProps = BaseProps & BreakpointProps<'row' | 'column' | 'row-reverse' | 'column-reverse'> & BaseProps;
+export type RowProps = BreakpointProps<'row' | 'column' | 'row-reverse' | 'column-reverse'> & BaseProps;
 
 export type RowState = {
   classes: { base: string };
