7e4bc2add148053d62bcc7eea22290c2016ccaac
Quique Fdez Guerra <quique@ckgrafico.com>
2023-03-21 09:27:56 +0200

    make breakpoint interface optional

--- src/models/breakpoint.model.ts
+++ src/models/breakpoint.model.ts
@@ -10,14 +10,14 @@ export enum Breakpoint {
 }
 
 export interface BreakpointProps<T> {
-  basic: T;
-  xxs: T;
-  xs: T;
-  s: T;
-  m: T;
-  l: T;
-  xl: T;
-  xxl: T;
+  basic?: T;
+  xxs?: T;
+  xs?: T;
+  s?: T;
+  m?: T;
+  l?: T;
+  xl?: T;
+  xxl?: T;
 }
 
 export const breakpoints = Object.entries(Breakpoint).map(([key, value]: [string, string]) => ({ key, value }));
